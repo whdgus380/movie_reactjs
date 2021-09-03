5b8819c3b7353885b486c25ae1fb9fb231afd37b
taekyeong <whdgus4615@gmail.com>
2021-09-03 11:39:49 +0900

    third

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import Potato from "./Potato";
+import App4 from "./App4";
 
-ReactDOM.render(<App />, document.getElementById("potato"));
+ReactDOM.render(<App4 />, document.getElementById("potato"));
